ab1569f19fc512521b05e3def63a01c43b4dc248
jiro4989 <jiroron666@gmail.com>
2021-01-22 09:46:03 +0900

    update go

--- 02_config/app/main.go
+++ 02_config/app/main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+  "os"
+  "io/ioutil"
+  "strconv"
   "fmt"
   "net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Hello, World")
+
+  b, err := ioutil.ReadFile("/app/data.json")
+  if err != nil {
+    panic(err)
+  }
+  fmt.Fprintf(w, string(b))
 }
 
 func main() {
+  port := os.Getenv("SERVER_PORT")
+
+  // validation
+  _, err := strconv.Atoi(port)
+  if err != nil {
+    panic(err)
+  }
+
   http.HandleFunc("/", handler)
-  http.ListenAndServe(":1234", nil)
+  http.ListenAndServe(":" + port, nil)
 }
