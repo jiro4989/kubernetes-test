e355f70ef88ca6f98a61efb26d7bdfa7ffea5462
jiro4989 <jiroron666@gmail.com>
2021-01-31 02:51:56 +0900

    test update

--- 06_skaffold/internal-api/main.nim
+++ 06_skaffold/internal-api/main.nim
@@ -5,7 +5,7 @@ from os import getEnv
 var server = newAsyncHttpServer()
 
 proc callback(req: Request) {.async.} =
-  await req.respond(Http200, "Hello Nim internal")
+  await req.respond(Http200, "Hello Nim internal and skaffold")
 
 let port = getEnv("SERVER_PORT").parseInt
 waitFor server.serve(Port(port), callback)
