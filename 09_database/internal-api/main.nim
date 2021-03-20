c9674225bdf181989286441510db83e61d02e9d5
jiro4989 <jiroron666@gmail.com>
2021-03-20 13:26:45 +0900

    DBに接続する処理をアプリに追加

--- 09_database/internal-api/main.nim
+++ 09_database/internal-api/main.nim
@@ -1,11 +1,22 @@
-import asynchttpserver, asyncdispatch
+import asynchttpserver, asyncdispatch, db_mysql
 from strutils import parseInt
 from os import getEnv
 
-var server = newAsyncHttpServer()
+var
+  server = newAsyncHttpServer()
+
+let
+  host = getEnv("DB_HOST")
+  user = getEnv("DB_USER")
+  password = getEnv("DB_PASSWORD")
+  dbName = getEnv("DB_NAME")
 
 proc callback(req: Request) {.async.} =
-  await req.respond(Http200, "Hello Nim internal and skaffold")
+  {.gcsafe.}:
+    let db = open(host, user, password, dbName)
+  let rows = db.getAllRows(sql"SELECT id, name, email FROM users")
+  defer: db.close
+  await req.respond(Http200, "Hello Nim internal and skaffold" & $rows)
 
 let port = getEnv("SERVER_PORT").parseInt
 waitFor server.serve(Port(port), callback)
