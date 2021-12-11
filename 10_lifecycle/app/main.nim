6c10824380a9bc97ca7cc003a188e5215c4d8ced
jiro4989 <jiroron666@gmail.com>
2021-12-11 16:21:06 +0900

    10をさくせいするために09をコピーだけした

---
+++ 10_lifecycle/app/main.nim
@@ -0,0 +1,24 @@
+import asynchttpserver, asyncdispatch, httpclient
+from strutils import parseInt
+from os import getEnv
+from strformat import `&`
+
+let
+  port = getEnv("SERVER_PORT").parseInt
+  inHost = getEnv("INTERNAL_API_HOST")
+  inPort = getEnv("INTERNAL_API_PORT").parseInt
+  userName = getEnv("USERNAME")
+  password = getEnv("PASSWORD")
+  client = newHttpClient()
+
+var
+  server = newAsyncHttpServer()
+
+proc callback(req: Request) {.async.} =
+  let data = readFile("/etc/app/config.json")
+  {.gcsafe.}:
+    let cont = client.getContent(&"{inHost}:{inPort}") & "\nUSERNAME = " &
+        userName & ", PASSWORD = " & password
+  await req.respond(Http200, "Hello Nim and skaffold\n" & data & cont)
+
+waitFor server.serve(Port(port), callback)
